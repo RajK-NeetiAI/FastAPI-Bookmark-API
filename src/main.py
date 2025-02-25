1db602d813b4142c36b32622e0b186eba2309b3f
RajK-NeetiAI <raj@neeti.ai>
2025-02-25 16:29:17 +0530

    added summary and description to the openapi

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 from src.routers import auth, bookmarks, home
 
-app = FastAPI(title="Bookmarks API")
+app = FastAPI(title="Bookmarks API", summary="This is the summary of the application.",
+              description="This is the description of the application.")
 
 app.include_router(auth.router)
 app.include_router(bookmarks.router)
